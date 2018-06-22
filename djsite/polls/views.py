58f9e5a7aed42271c572066a18648e91788643a9
chongliu@cn.ibm.com <chongliu@cn.ibm.com>
2018-06-22 18:29:23 +0800

    Add template

--- djsite/polls/views.py
+++ djsite/polls/views.py
@@ -1,10 +1,20 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question
+from django.template import loader
+from django.shortcuts import render
 
 def index(request):
     """The function's docstring"""
-    return HttpResponse("Hello kitty, you are in the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    print(latest_question_list)
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
