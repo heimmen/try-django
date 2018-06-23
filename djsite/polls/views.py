a89ecca668e603f34600a3e2f3efdbb2fc6cb403
CHONG LIU <chongliu@cn.ibm.com>
2018-06-23 16:10:24 +0800

    clean code in views.py

--- djsite/polls/views.py
+++ djsite/polls/views.py
@@ -8,13 +8,10 @@ from django.shortcuts import render, get_object_or_404
 def index(request):
     """The function's docstring"""
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
     print(latest_question_list)
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(template.render(context, request))
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
@@ -24,11 +21,8 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
-    # response = "You are looking at the results of qeustion %s."
-    # return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    # return HttpResponse("You are voting on question %s." % question_id)
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
